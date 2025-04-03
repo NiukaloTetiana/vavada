61cff161d1d29fd3d33545372319abfebcbd73d8
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-04-03 18:26:04 +0300

    initial files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+import throttle from "lodash.throttle";
+
 (() => {
   const mobileMenu = document.querySelector(".js-backdrop");
   const openMenuBtn = document.querySelector(".js-menu__open");
@@ -31,3 +33,26 @@
     }
   });
 })();
+   
+
+const scrollUpButton = document.querySelector(".js-scroll-up-btn");
+
+const threshold = 300;
+
+document.addEventListener(
+  "scroll",
+  throttle(() => {
+    if (window.scrollY > threshold) {
+      scrollUpButton.classList.add("show");
+    } else {
+      scrollUpButton.classList.remove("show");
+    }
+  }, 500)
+);
+
+scrollUpButton.addEventListener("click", () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+});
